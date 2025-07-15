fff25c43c94f24882b47bc3a410e00fe3b123b5a
Max Lehmann <max.lehmann@rumble.de>
2025-07-16 00:16:53 +0200

    Added: SiteController to test server setup & object parameter for mocking

--- test-utils/setup-server.ts
+++ test-utils/setup-server.ts
@@ -2,20 +2,37 @@
 import type { LoggerInterface } from 'src/components/logger/LoggerInterface';
 import Server from 'src/components/server/Server';
 import IndexController from 'src/controllers/IndexController';
+import SiteController from 'src/controllers/SiteController';
+import PluginRepository from 'src/repositories/PluginRepository';
+import SiteRepository from 'src/repositories/SiteRepository';
+import LatestVersionResolver from 'src/services/latest-version/LatestVersionResolver';
 
-const mockLogger: LoggerInterface = {
-    info: jest.fn(),
-    warn: jest.fn(),
-    error: jest.fn(),
-    debug: jest.fn(),
-    silly: jest.fn(),
-};
-
-export async function setupTestServer() {
+export async function setupTestServer({
+    logger = {
+        info: jest.fn(),
+        warn: jest.fn(),
+        error: jest.fn(),
+        debug: jest.fn(),
+        silly: jest.fn(),
+    } as LoggerInterface,
+    siteRepository = {} as jest.Mocked<SiteRepository>,
+    pluginRepository = {} as jest.Mocked<PluginRepository>,
+    latestVersionResolver = {} as jest.Mocked<LatestVersionResolver>,
+}: {
+    logger?: LoggerInterface;
+    siteRepository?: jest.Mocked<SiteRepository>;
+    pluginRepository?: jest.Mocked<PluginRepository>;
+    latestVersionResolver?: jest.Mocked<LatestVersionResolver>;
+} = {}) {
     Server.setConfig({ port: 0, corsOptions: {} });
+    Server.resetInstance();
 
-    const serverInstance = Server.getInstance(mockLogger);
-    serverInstance.useRouter('/', new IndexController(mockLogger).getRouter());
+    const serverInstance = Server.getInstance(logger);
+    serverInstance.useRouter('/', new IndexController(logger).getRouter());
+    serverInstance.useRouter(
+        '/site',
+        new SiteController(logger, siteRepository, pluginRepository, latestVersionResolver).getRouter()
+    );
 
     const app = serverInstance.getApp();
     return { app, server: serverInstance };
