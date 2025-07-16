4efc162e90d36616dec510eabda4b7f8034107e5
Max Lehmann <max.lehmann@rumble.de>
2025-07-16 08:54:55 +0200

    Added: more tests for SchedulerLogger

--- src/components/logger/SchedulerLogger.spec.ts
+++ src/components/logger/SchedulerLogger.spec.ts
@@ -19,4 +19,46 @@ describe('SchedulerLogger', () => {
             expect(transport.dirname).toBe(baseDir);
         });
     });
+
+    describe('SchedulerLogger.logger.fileFormat', () => {
+        beforeAll(() => {
+            jest.useFakeTimers();
+            jest.setSystemTime(new Date('2025-01-01:00:00:00Z'));
+        });
+        afterAll(() => {
+            jest.useRealTimers();
+        });
+
+        it('should format the log message without meta', () => {
+            const config: LoggerConfig = { directory: baseDir, level: 'info' };
+            const schedulerLogger = new SchedulerLogger(config);
+            const winstonLogger = schedulerLogger['logger'] as winston.Logger;
+            const [transport] = winstonLogger.transports as DailyRotateFile[];
+
+            const transformed = transport.format?.transform({
+                level: 'info',
+                message: 'Test message',
+            }) as winston.Logform.TransformableInfo;
+
+            const output = transformed[Symbol.for('message')] as string;
+            expect(output).toBe('2025-01-01 00:00:00 [info] Test message ');
+        });
+
+        it('should format the log message with meta', () => {
+            const config: LoggerConfig = { directory: baseDir, level: 'info' };
+            const schedulerLogger = new SchedulerLogger(config);
+            const winstonLogger = schedulerLogger['logger'] as winston.Logger;
+            const [transport] = winstonLogger.transports as DailyRotateFile[];
+
+            const transformed = transport.format?.transform({
+                level: 'info',
+                message: 'Test message',
+                timestamp: '2025-01-01 00:00:00', // gets overtaken by jest.setSystemTime
+                key: 'value',
+            }) as winston.Logform.TransformableInfo;
+
+            const output = transformed[Symbol.for('message')] as string;
+            expect(output).toBe('2025-01-01 00:00:00 [info] Test message {"key":"value"}');
+        });
+    });
 });
